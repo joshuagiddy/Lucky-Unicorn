555d6f1a29f5de173782cdf6544cc56dd3819fe2
joshuagiddy <giddyj@middleton.school.nz>
2023-04-22 22:49:34 +1200

    Rounds

--- 00_LU_base_v2.py
+++ 00_LU_base_v2.py
@@ -1,7 +1,6 @@
 """LU Base component
 Components added after they have been created and tested"""
 
-import random
 
 # Functions go here...
 def yes_no(question_text):
@@ -52,40 +51,6 @@ def num_check(question, low, high):
         except ValueError:
             print(error)
 
-def generate_token(BALANCE):
-
-    rounds_played = 0
-    play_again = ""
-
-    while play_again != "x":
-        rounds_played += 1
-        number = random.randint(1, 100)
-
-        if 1 <= number <= 5:
-            token = "unicorn"
-            BALANCE += 4
-        elif 6 <= number <= 36:
-            token = "donkey"
-            BALANCE -= 1
-        else:
-            if number % 2 == 0:
-                token = "zebra"
-                BALANCE -= 0.5
-            else:
-                token = "horse"
-                BALANCE -= 0.5
-
-        print(f"Rounds {rounds_played}, Tokens: {token}, Balance: ${BALANCE:.2f}")
-        if BALANCE < 1:
-            print("\nSorry but you have run out of money")
-            play_again = "x"
-        else:
-            play_again = input("\nDo you want to play another round\n<enter> to play"
-                               " again or 'X' to exit ").lower()
-        print()
-    return BALANCE
-
-
 # Main routine go here...
 played_before = yes_no("Have you played this game before? ")
 
@@ -96,12 +61,5 @@ else:
 
 
 
-starting_balance = num_check("How much would you like to pay with? $", 1, 10)
-print(f"You are plaing with ${starting_balance}")
-
-starting_balance = 5
-closing_balance = generate_token(starting_balance)
-print("Thanks for playing")
-print(f"You have started with ${starting_balance:.2f}")
-print(f"and leave with ${closing_balance:.2f}")
-print("Goodbye")
+user_balance = num_check("How much would you like to pay with? $", 1, 10)
+print(f"You are plaing with ${user_balance}")
---
+++ 06_rounds_v3.py
@@ -0,0 +1,49 @@
+"""Component 4 - game mechanics and looping v2
+tokens are allocated from a random radint of 1, 100.
+Gives us feedback on our balance and number of rounds.
+"""
+import random
+
+# Main routine
+def generate_token(BALANCE):
+
+    rounds_played = 0
+    play_again = ""
+
+    while play_again != "x":
+        rounds_played += 1
+        number = random.randint(1, 100)
+
+        if 1 <= number <= 5:
+            token = "unicorn"
+            BALANCE += 4
+        elif 6 <= number <= 36:
+            token = "donkey"
+            BALANCE -= 1
+        else:
+            if number % 2 == 0:
+                token = "zebra"
+                BALANCE -= 0.5
+            else:
+                token = "horse"
+                BALANCE -= 0.5
+
+        print(f"Rounds {rounds_played}, Tokens: {token}, Balance: ${BALANCE:.2f}")
+        if BALANCE < 1:
+            print("\nSorry but you have run out of money")
+            play_again = "x"
+        else:
+            play_again = input("\nDo you want to play another round\n<enter> to play"
+                               " again or 'X' to exit ").lower()
+        print()
+    return BALANCE
+
+starting_balance = 5
+closing_balance = generate_token(starting_balance)
+print("Thanks for playing")
+print(f"You have started with ${starting_balance:.2f}")
+print(f"and leave with ${closing_balance:.2f}")
+print("Goodbye")
+
+
+

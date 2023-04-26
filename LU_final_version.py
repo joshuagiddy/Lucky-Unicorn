7b1a9878960dee7a929a8731450c314271657db3
joshuagiddy <giddyj@middleton.school.nz>
2023-04-27 11:24:11 +1200

    Final Version

--- LU_final_version.py
+++ LU_final_version.py
@@ -3,6 +3,7 @@ Components added after they have been created and tested"""
 
 import random
 
+
 # Functions go here...
 def yes_no(question_text):
     while True:
@@ -24,7 +25,8 @@ def yes_no(question_text):
         else:
             print("error, please answer 'yes' or 'no'")
 
-#function to display instructions
+
+# function to display instructions
 def instructions():
     print()
     print(formatter("*", "How to play"))
@@ -42,7 +44,8 @@ def instructions():
     print("*" * 50)
     print()
 
-#number checking function
+
+# number checking function
 def num_check(question, low, high):
     error = "That was not valid\n" \
             "Please enter a number between {} and {}\n".format(low,high)
@@ -62,7 +65,8 @@ def num_check(question, low, high):
         except ValueError:
             print(error)
 
-def generate_token(BALANCE):
+
+def generate_token(balance):
 
     rounds_played = 1
     play_again = ""
@@ -73,38 +77,40 @@ def generate_token(BALANCE):
         number = random.randint(1, 100)
 
         if 1 <= number <= 5:
-            BALANCE += 4
+            balance += 4
             print(formatter("!", "Congratulations you get a Unicorn"))
             print()
         elif 6 <= number <= 36:
-            BALANCE -= 1
+            balance -= 1
             print(formatter("D", "Bad lucky, you get a donkey"))
             print()
         else:
             if number % 2 == 0:
-                BALANCE -= 0.5
+                balance -= 0.5
                 print(formatter("Z", "Bad lucky, you get a zebra"))
                 print()
             else:
-                BALANCE -= 0.5
+                balance -= 0.5
                 print(formatter("H", "Bad luck, you got a horse"))
                 print()
 
-        print(f"Your balance is now ${BALANCE:.2f}")
-        if BALANCE < 1:
+        print(f"Your balance is now ${balance:.2f}")
+        if balance < 1:
             print("\nSorry but you have ran out of money")
             play_again = "x"
         else:
             play_again = input("\nDo you want to play another round\n<enter> to play"
                                "again or 'X' to exit ").lower()
         print()
-    return BALANCE
+    return balance
+
 
 def formatter(symbol, text):
     sides = symbol * 3
     formatted_text = f"{sides} {text} {sides}"
     top_bottom = symbol * len(formatted_text)
     return f"{top_bottom}\n{formatted_text}\n{top_bottom}"
+
 
 # Main routine go here...
 print(formatter("-", "Welcome to the lucky Unicorn Game"))
@@ -115,7 +121,6 @@ if played_before == "No":
     instructions()
 else:
     print("Program continues")
-
 
 
 starting_balance = num_check("How much would you like to pay with? $", 1, 10)
